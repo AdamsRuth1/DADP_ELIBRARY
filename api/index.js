bf1731b86dd82a5338e80b22ff45a7adb2a5f937
ADAMS <adamsruth4080@gmail.com>
2026-04-28 15:15:45 +0100

    Add crash protection to backend bridge for better diagnostics

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-const { app, loadDB } = require('../server/app');
+try {
+  const { app, loadDB } = require('../server/app');
 
-if (typeof loadDB === 'function') {
-  loadDB();
+  if (typeof loadDB === 'function') {
+    loadDB();
+  }
+
+  module.exports = app;
+} catch (err) {
+  const express = require('express');
+  const app = express();
+  app.all('*', (req, res) => {
+    res.status(500).json({ 
+      error: 'Backend Bridge Crash', 
+      details: err.message,
+      stack: err.stack 
+    });
+  });
+  module.exports = app;
 }
-
-module.exports = app;
